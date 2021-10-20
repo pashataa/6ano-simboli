0f6a516b2c5b240213f579a7b1557fdbe1f5691f
User 12 <User 12@PC17>
2021-10-20 14:50:07 +0300

    Add project files.

---
+++ 6ano simboli/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _6ano_simboli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(str = str.Replace(" ", String.Empty));
+        }
+    }
+}
